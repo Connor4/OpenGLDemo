c0c5eeba308ba4d756ba86aba8ac07c010f0b887
邓智彬 <dzb@meitu.com>
2016-09-02 09:59:26 +0800

    清除当前分支

--- src/main/res/raw/point_texture_fragment_shader.glsl
+++
@@ -1,9 +0,0 @@
-precision highp float;
-uniform sampler2D u_TextureUnit;
-
-varying vec2 v_TextureCoordinates;
-void main()
-{
-       vec4 pointcolor = texture2D(u_TextureUnit, v_TextureCoordinates);
-      gl_FragColor = vec4(1.0, 0.0, 0.0, pointcolor.r);
-}
--- src/main/res/raw/texture_fragment_shader.glsl
+++
@@ -1,9 +0,0 @@
-precision highp float;
-
-uniform sampler2D u_TextureUnit;
-varying vec2 v_TextureCoordinates;
-
-void main()
-{
-    gl_FragColor = texture2D(u_TextureUnit, v_TextureCoordinates);
-}
